a295e58f19434492e0b44f510bd74ba482bb8825
Romanhey <romanmankevich09@gmail.com>
2025-05-03 20:15:42 +0300

    Adds proj files

--- VBALL.Identity/Identity.Presentation/obj/Debug/net8.0/Identity.Presentation.AssemblyInfo.cs
+++ VBALL.Identity/Identity.Presentation/obj/Debug/net8.0/Identity.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Identity.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c37873b59fbe80459f1292873d0eb6d63f28f472")]
 [assembly: System.Reflection.AssemblyProductAttribute("Identity.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Identity.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- VBALL.Schedule/Schedule.Presentation/obj/Release/net8.0/Schedule.Presentation.AssemblyInfo.cs
+++ VBALL.Schedule/Schedule.Presentation/obj/Release/net8.0/Schedule.Presentation.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Schedule.Presentation")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c37873b59fbe80459f1292873d0eb6d63f28f472")]
 [assembly: System.Reflection.AssemblyProductAttribute("Schedule.Presentation")]
