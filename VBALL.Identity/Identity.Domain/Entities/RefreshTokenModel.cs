fb4469db465e8f0f75c81599792cbde839e6352b
Romanhey <romanmankevich09@gmail.com>
2025-05-20 21:00:28 +0300

    add RefreshToken and RefreshTokenRepository

---
+++ VBALL.Identity/Identity.Domain/Entities/RefreshTokenModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Identity.Domain.Entities
+{
+    public class RefreshTokenModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public string? RefreshToken { get; set; }
+
+        public DateTime RefreshTokenExpireDate { get; set; }
+
+        public User? User { get; set; }
+    }
+
+}
--- VBALL.Identity/Identity.Infrasructure/Persistance/ApplicationDbContext.cs
+++ VBALL.Identity/Identity.Infrasructure/Persistance/ApplicationDbContext.cs
@@ -7,5 +7,7 @@ namespace Identity.Infastucture.Persistance
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<User> Users { get; set; }
+
+        public DbSet<RefreshTokenModel> RefreshTokens { get; set; }
     }
 }
