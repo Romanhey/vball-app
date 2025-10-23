8f88e9dfca77b33e0bd5f4e9158184c34dff26cc
Romanhey <romanmankevich09@gmail.com>
2025-10-23 13:04:33 +0300

    add application dependencies

---
+++ VBALL.Schedule/Schedule.Application/DI/ApplicationDependencies.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Schedule.Application.MappingProfiles;
+using Schedule.Application.UseCases.Match.CreateMatch;
+namespace Schedule.Application.DI;
+public static class ApplicationDependencies
+{
+    public static IServiceCollection AddApplicationService(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateMatchComandHandler).Assembly));
+        services.AddAutoMapper(cfg =>
+        {
+            cfg.AddProfile<MatchProfile>();
+        });
+
+        return services;
+    }
+}
