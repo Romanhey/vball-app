11ae28554554633216eb7e1849f7105386791912
Romanhey <romanmankevich09@gmail.com>
2025-11-01 14:33:56 +0300

    adds exception's middleware

---
+++ VBALL.Schedule/Schedule.Application/Exeptions/AlreadyExistException.cs
@@ -0,0 +1,11 @@
+﻿namespace Schedule.Application.Exeptions
+{
+    public class AlreadyExistException : Exception
+    {
+        private const string DefaultMessage = "The request is already exist.";
+
+        public AlreadyExistException(): base(DefaultMessage) { }
+        public AlreadyExistException(string message) : base(message) { }
+        public AlreadyExistException(string message,  Exception innerException) : base(message, innerException) { }
+    }
+}
