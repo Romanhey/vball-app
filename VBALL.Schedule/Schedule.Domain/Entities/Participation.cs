a34f9cffd33811c963706c6403d2a4b7dc2d1f4f
Alex Usov <usov.lesha2017@gmail.com>
2025-11-05 23:15:56 +0300

    feat(domain): add CancellationReason field to Participation entity

--- VBALL.Schedule/Schedule.Domain/Entities/Participation.cs
+++ VBALL.Schedule/Schedule.Domain/Entities/Participation.cs
@@ -5,11 +5,12 @@ namespace Schedule.Domain.Entities
     public class Participation
     {
         [Key]
-        public required int ParticipationId {  get; set; }
+        public required int ParticipationId { get; set; }
         public required int MatchId { get; set; }
-        public required int PlayerId {  get; set; }
+        public required int PlayerId { get; set; }
         public required DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public required ParticipationStatus Status { get; set; }
+        public string? CancellationReason { get; set; }
     }
 }
